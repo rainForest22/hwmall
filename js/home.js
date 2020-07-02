e1a010424075e53c97ee48bb095ba8b55ec2a9a4
rainForest22 <61275880+rainForest22@users.noreply.github.com>
2020-07-02 10:28:54 +0800

    修复bug

--- js/home.js
+++ js/home.js
@@ -1,10 +1,7 @@
 $(function () {  
-    // 生成轮播图
-    console.log(2);
-    
+    // 生成轮播图    
     new Slider($(".banner"),"http://localhost/hwmall/sever/data/sliderImg.json").init();
     // 生成清单表
     new CateList($(".catelist"),"http://localhost/hwmall/sever/data/cateList.json").init();
-    console.log(CateList);
     
 })
