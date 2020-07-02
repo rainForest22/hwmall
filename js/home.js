114a566bf905b67004fc75efc17ba4979c1e68b5
rainForest22 <61275880+rainForest22@users.noreply.github.com>
2020-07-02 10:24:48 +0800

    修改

--- js/home.js
+++ js/home.js
@@ -2,7 +2,7 @@ $(function () {
     // 生成轮播图
     console.log(2);
     
-    // new Slider($(".banner"),"http://localhost/hwmall/sever/data/sliderImg.json").init();
+    new Slider($(".banner"),"http://localhost/hwmall/sever/data/sliderImg.json").init();
     // 生成清单表
     new CateList($(".catelist"),"http://localhost/hwmall/sever/data/cateList.json").init();
     console.log(CateList);
